71c8db305850a2ad43bbf99ef5f227eeebddf5f0
Bárbara <babivaldez@gmail.com>
2023-11-05 20:30:03 -0300

    req.02

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,22 @@
+# Esta função é capaz de importar notícias a partir de um arquivo TXT,
+# utilizando "\n" como separador.
+# O caractere de nova linha é \n .
+# Ele é usado para indicar o final de uma linha de texto.
+
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        with open(path_file) as file:
+            if path_file.endswith(".txt"):
+                return file.read().split("\n")
+            else:
+                sys.stderr.write("Formato inválido\n")
+                return None
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return None
+
+
+# print(txt_importer("sd-030-a-project-ting/ting_file_management/exemplo.txa"))
--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,16 @@
+# Essa função é capaz de transformar o conteúdo da lista
+# gerada pela função txt_importer
+# em um dicionário que será armazenado dentro da fila.
 def process(path_file, instance):
     """Aqui irá sua implementação"""
 
 
+# Essa função é capaz de remover o primeiro arquivo processado
 def remove(instance):
     """Aqui irá sua implementação"""
 
 
+# Essa função é capaz de apresentar as informações
+# superficiais de um arquivo processado.
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
