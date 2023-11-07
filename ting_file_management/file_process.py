a96164a3d90e920c8d72277e0d653a05e193ad47
Bárbara <babivaldez@gmail.com>
2023-11-07 17:00:55 -0300

    req01

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,10 @@
 # Essa função é capaz de transformar o conteúdo da lista
 # gerada pela função txt_importer
 # em um dicionário que será armazenado dentro da fila.
+
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
 
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,20 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self._data = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._data)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self._data) != 0:
+            return self._data.pop(0)
+        return None
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= len(self._data) or index < 0:
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self._data[index]
