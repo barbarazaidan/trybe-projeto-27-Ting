0f711be243e7b1228a13eb962acf14289f71c2ef
Bárbara <babivaldez@gmail.com>
2023-11-08 15:10:03 -0300

    req.03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,11 +2,28 @@
 # gerada pela função txt_importer
 # em um dicionário que será armazenado dentro da fila.
 
-# from ting_file_management.file_management import txt_importer
+from ting_file_management.file_management import txt_importer
 
 
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list_content = txt_importer(path_file)
+
+    # faço uma iteração sobre os elementos da fila
+    for index in range(instance.__len__()):
+        # acesso o elemento da fila através do método search
+        element = instance.search(index)
+        # verifico se o nome do arquivo já está na fila
+        if element["nome_do_arquivo"] == path_file:
+            return None
+
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(list_content),
+        "linhas_do_arquivo": list_content,
+    }
+
+    instance.enqueue(new_dict)
+    print(new_dict)
 
 
 # Essa função é capaz de remover o primeiro arquivo processado
